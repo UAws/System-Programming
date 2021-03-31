64b7cfa0c6f24bfb2c26a48c28059148d1b7b261
Akide <mail@llycloud.com>
2021-03-31 17:00:19 +1030

    assignment 2 Round 1: sequence.c

---
+++ assignment/assignment2/pipeline.c
@@ -0,0 +1,11 @@
+// Author : Akide Liu 
+// Date : 31/3/21
+// License and copyright notice: GNU General Public License v3.0 
+// https://www.gnu.org/licenses/gpl-3.0.en.html
+// Description : 
+
+//
+
+int main(int argc, char **argv){
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "stretchy_buffer.h"
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+    // printf("%s", "4 2020");
+    // char *args[] = {"cal", "4", "2020", NULL};
+    //
+    // execvp("cal", args);
+
+
+    double *v = 0;
+            sb_push(v, 1.0);
+            sb_push(v, 42.0);
+    for(int i = 0; i < sb_count(v); ++i)
+        printf("%g\n", v[i]);
+            sb_free(v);
 }
